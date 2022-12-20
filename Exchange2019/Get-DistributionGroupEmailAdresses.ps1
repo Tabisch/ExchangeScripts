1be5a152bef7f1a80c1a4f720f3a6bd80f227966
Tabisch <tobithieman@gmail.com>
2022-12-20 13:30:24 +0100

    Add files via upload

---
+++ Exchange2019/Get-DistributionGroupEmailAdresses.ps1
@@ -0,0 +1,23 @@
+﻿$path = ".\distribution_smtpadresses.csv"
+
+Remove-Item -Path $path -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+
+#Head
+$output = "`"DisplayName`",`"PrimarySmtpAddress`",`"SmtpAddress`""
+$output | Out-File -FilePath $path -Encoding utf8 -Append
+
+$distributionGroup = Get-DistributionGroup 
+
+$distributionGroup | ForEach-Object{
+
+    Write-Host $_.DisplayName
+
+    $addresses = $_.EmailAddresses | Where-Object{ $_.Prefix.PrimaryPrefix -contains "SMTP" } | Where-Object{ $_.SmtpAddress -notmatch ".onmicrosoft.com" } | Where-Object{ $_.SmtpAddress -notmatch "DiscoverySearchMailbox" }
+
+    foreach($address in $addresses)
+    {
+        $output = "`"$($_.DisplayName)`",`"$($_.PrimarySmtpAddress)`",`"$($address.SmtpAddress)`""
+        #Write-Host $output
+        $output | Out-File -FilePath $path -Encoding utf8 -Append
+    }
+}
---
+++ Exchange2019/Get-MailboxEmailAdresses.ps1
@@ -0,0 +1,23 @@
+﻿$path = ".\mailboxes_smtpadresses.csv"
+
+Remove-Item -Path $path -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+
+#Head
+$output = "`"DisplayName`",`"PrimarySmtpAddress`",`"SmtpAddress`""
+$output | Out-File -FilePath $path -Encoding utf8 -Append
+
+$mailboxes = Get-Mailbox 
+
+$mailboxes | ForEach-Object{
+
+    Write-Host $_.DisplayName
+
+    $addresses = $_.EmailAddresses | Where-Object{ $_.Prefix.PrimaryPrefix -contains "SMTP" } | Where-Object{ $_.SmtpAddress -notmatch ".onmicrosoft.com" } | Where-Object{ $_.SmtpAddress -notmatch "DiscoverySearchMailbox" }
+
+    foreach($address in $addresses)
+    {
+        $output = "`"$($_.DisplayName)`",`"$($_.PrimarySmtpAddress)`",`"$($address.SmtpAddress)`""
+        #Write-Host $output
+        $output | Out-File -FilePath $path -Encoding utf8 -Append
+    }
+}
